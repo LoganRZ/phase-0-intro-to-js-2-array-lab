1618dfeb8a049b24b76c781416770a39c38e6c94
Loganrayzander@gmail.com <loganzander@Logans-MacBook-Pro.local>
2022-10-14 12:38:44 -0500

    commit tree 4efbb84120b4fd65ad7974a635cf3fb6cc74fdde

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];{
+}
+function destructivelyAppendCat (name) { 
+    cats.push("Ralph")
+}
+function destructivelyPrependCat (name) { 
+    cats.unshift("Bob")
+}
+function destructivelyRemoveLastCat () {
+    cats.pop()  
+}
+function destructivelyRemoveFirstCat () {  
+    cats.shift()
+}
+function appendCat (name) {
+   return [...cats, name]
+}
+function prependCat (name) {
+    return [name, ...cats]
+}
+function removeLastCat () {
+    const newcats = [...cats]
+    newcats.pop()
+    return (newcats)
+}
+function removeFirstCat () {
+    const newcats = [...cats]
+    newcats.shift()
+    return (newcats)
+}
